ed6efc3aca64a897b53105e3881865430c2c8abd
dannyambarita <48155543+dannyambarita@users.noreply.github.com>
2021-06-01 21:07:41 +0700

    make it better

---
+++ src/Example1.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Example1 extends Component {
+	render() {
+		return (
+            <section className="atas">
+                <div>
+                    <ul id='head'>
+                        <li> Lake's Gaming Store</li>
+                    </ul>
+                </div>
+            </section>    
+        );
+    }
+} 
+export default Example1;
