c0af663308591bcf3fc37e6dd1b9fe2701bc63a1
dannyambarita <48155543+dannyambarita@users.noreply.github.com>
2021-06-01 22:00:37 +0700

    upload

--- praktikum4-app/src/App.js
+++ praktikum4-app/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+import React, { Component } from 'react';
+import Tampilan1 from './Tampilan1';
+import Tampilan2 from './Tampilan2';
+import Tampilan3 from './Tampilan3';
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Tampilan1></Tampilan1>
+        <Tampilan2></Tampilan2>
+        <Tampilan3></Tampilan3>
+      </div>
+      
+    );
+  }
 }
-
+ 
 export default App;
---
+++ praktikum4-app/src/Tampilan1.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Tampilan1 extends Component {
+	render() {
+		return (
+            <section className="atas">
+                <div>
+                    <ul id='head'>
+                        <li>Toko Bunga Vera</li>
+                    </ul>
+                </div>
+            </section>    
+        );
+    }
+} 
+export default Tampilan1;
